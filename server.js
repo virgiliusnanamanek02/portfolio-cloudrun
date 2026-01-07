c7cc5ba83d4e834a60d92dcd4135ced19d90db18
Virgilius Nana Manek <virgilius.nana.manek0719@gmail.com>
2026-01-07 23:22:20 +0700

    feat: implement gemini ai

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.APP_PORT || 8000;
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/ask", async (req, res) => {
@@ -19,11 +25,35 @@ app.post("/ask", async (req, res) => {
     return res.status(400).json({ answer: "Please ask something." });
   }
 
-  // GEMINI API
-  return res.json({
-    answer:
-      "This portfolio focuses on backend decision-making and architectural trade-offs.",
-  });
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-3-flash-preview",
+      contents: `
+You are an AI assistant for a backend developer portfolio.
+
+Developer: Vicky Nana
+Focus: backend systems, architectural decisions, trade-offs.
+
+User question:
+${question}
+
+Answer clearly, concisely, and practically.
+      `,
+    });
+
+    res.json({ answer: response.text });
+  } catch (err) {
+    if (err.status === 503) {
+      return res.json({
+        answer:
+          "The AI service is temporarily busy. Please try again in a moment.",
+      });
+    }
+
+    res.status(500).json({
+      answer: "Unexpected error occurred.",
+    });
+  }
 });
 
 app.listen(PORT, () => {
